32812816cc31849e80d316874f9472c331fe1de5
Mohamed Aly Sidibe <mouhamed.aly.sidibe96@gmail.com>
2020-07-01 20:34:49 +0000

    Expo electron app init

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, Button } from "react-native";
 
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
+  const sayHello = () => {
+    alert("hello guys");
+  };
   return (
-    <LinearGradient colors={['white', '#3423ca']} style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <LinearGradient colors={["orange", "red"]} style={styles.container}>
+      <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+        Hey electron app!
+      </Text>
+      <Button title="click me" onPress={sayHello} />
     </LinearGradient>
   );
 }
@@ -14,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
